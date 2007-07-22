ac726a2dd83df4c86802e6568465eead67007cc2
Jed Frechette <jdfrech@unm.edu>
2007-07-22 18:21:06 +0000

    wakeiceb.py: Do everything in Python.

--- wakeiceb.py
+++ wakeiceb.py
@@ -3,17 +3,27 @@
 
 __author__ = 'Jed Frechette <jedfrechette@gmail.com>'
 __version__ = '0.1'
-__date__ = '26 June 2007'
+__date__ = '22 July 2007'
 
-from socket import gethostbyname
-from subprocess import call
+import socket
+import struct
 
 if __name__ == '__main__':
     hostname = 'iceb.ath.cx'
-    ip = gethostbyname(hostname)
-    hw_addr = '00:02:2A:D6:B5:FC'
+    ip = socket.gethostbyname(hostname)
+    hw_addr = '00022AD6B5FC'
 
-    cmd = 'wakeonlan'
-    cmd_opts = '-i %s' %ip
+    print "Sending magic packet to %s with %s" % (ip, hw_addr)
+    # Pad the synchronization stream.
+    data = ''.join(['FFFFFFFFFFFF', hw_addr * 20])
+    send_data = '' 
 
-    call([cmd, cmd_opts, hw_addr])
+    # Split up the hex values and pack.
+    for i in range(0, len(data), 2):
+        send_data = ''.join([send_data,
+                             struct.pack('B', int(data[i: i + 2], 16))])
+
+    # Broadcast it to the LAN.
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.sendto(send_data, (ip, 9))
