6c4092a61a24eac89fe3a1c7cb87a8168e6ae6fa
Jed Frechette <jdfrech@unm.edu>
2008-01-23 17:44:07 +0000

    xyzs2vtp.py: Refactored to get rid of pylint global wname warnings.

--- xyzs2vtp.py
+++ xyzs2vtp.py
@@ -8,30 +8,35 @@ The results are saved to an output file with the same base name as the input
 file and the extension vtp."""
 
 __author__ = "Jed Frechette <jdfrech@unm.edu>"
-__date__ = "27 September 2007"
+__date__ = "23 January 2008"
 __version__ = "0.1"
+__license__ = "BSD <http://opensource.org/licenses/bsd-license.php>"
 
 from glob import glob
 from optparse import OptionParser
 from os import name, path
 from vtk import vtkParticleReader, vtkXMLPolyDataWriter
 
-if __name__ == '__main__':
+def get_filenames():
+    """Return a list of filenames to process."""
     parser = OptionParser(usage='%prog INPUT_FILES',
                           description=' '.join(__doc__.split()),
                           version=__version__)
     (opts, args) = parser.parse_args()
     if name == 'nt':
         args = glob(args[0])
+    return args
 
-    for input_file in args:
-        output_file = '.'.join([path.splitext(path.split(input_file)[-1])[0],
+
+if __name__ == '__main__':
+    for in_filename in get_filenames():
+        out_filename = '.'.join([path.splitext(path.split(in_filename)[-1])[0],
                                 'vtp'])
         reader = vtkParticleReader()
-        reader.SetFileName(input_file)
+        reader.SetFileName(in_filename)
         
         writer = vtkXMLPolyDataWriter()
-        writer.SetFileName(output_file)
+        writer.SetFileName(out_filename)
         writer.SetInputConnection(reader.GetOutputPort())
         writer.Write()
-        print "Saved file: %s" % output_file
+        print "Saved file: %s" % out_filename
