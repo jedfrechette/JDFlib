cee06daa65cb7c5f0a0308904fefea850b6ee054
Jed Frechette <jed@lidarguys.com>
2008-02-08 16:26:37 -0700

    Refactor angles to use simple floats.

--- cogo.py
+++ cogo.py
@@ -9,70 +9,66 @@ __license__ = "MIT <http://opensource.org/licenses/mit-license.php>"
 from types import IntType, FloatType
 from enthought.traits.api import HasTraits, Button, Float, Dict, Int
 from enthought.traits.ui.api import View, Item
-from scipy import arctan, cos, pi, sin, sqrt
+from scipy import arctan, ceil, cos, floor, pi, sin, sqrt
 import logging
 
-class PositiveFloat(Float):
-    def validate(self, object, name, value):
-        info_text = 'a float >= 0'
-        super(SecondFloat, self).validate(object, name, value)
-        if value >= 0:
-            return value 
-        self.error(object, name, value)
-        
-class PositiveInt(Int):
-    def validate(self, object, name, value):
-        info_text = 'an integer >= 0'
-        super(PositiveInt, self).validate(object, name, value)
-        if value >= 0:
-            return value 
-        self.error(object, name, value)
-
-class MinuteInt(Int):
-    def validate(self, object, name, value):
-        info_text = 'an integer >= 0 and < 60'
-        super(PositiveInt, self).validate(object, name, value)
-        if 60 > value >= 0:
-            return value 
-        self.error(object, name, value)
-    
-class SecondFloat(Float):
-    def validate(self, object, name, value):
-        info_text = 'a float >= 0 and < 60'
-        super(SecondFloat, self).validate(object, name, value)
-        if 60 > value >= 0:
-            return value 
-        self.error(object, name, value)
 
 class Angle(HasTraits):
     """An angle in decimal degrees or degrees, minutes seconds."""
-    decimal_degrees = PositiveFloat
-    degrees = PositiveInt
-    minutes = MinuteInt
-    seconds = SecondFloat
-    radians = PositiveFloat
+    decimal_degrees = Float
+    degrees = Float
+    minutes = Float
+    seconds = Float
+    radians = Float
     
-    def _decimal_degrees_changed(self):
+    def _dd2rad(self):
+        """Convert decimal degrees to radians."""
         self.radians = self.decimal_degrees * pi/180
-#        self.degrees = int(self.decimal_degrees)
-#        self.minutes = int(60 * (self.decimal_degrees - self.degrees))
-#        self.seconds = 60 * (60 * (self.decimal_degrees- self.degrees)
-#                             - self.minutes)
+    
+    def _dd2dms(self):
+        """Convert decimal degrees to degrees, minutes, seconds."""
+        self.degrees = floor(self.decimal_degrees)
+        self.minutes = floor(60 * (self.decimal_degrees - self.degrees))
+        self.seconds = 60 * (60 * (self.decimal_degrees - self.degrees)
+                             - self.minutes)
         
-    def _degrees_changed(self):
-        self.decimal_degrees = self.degrees + self.minutes/60.0 \
+    def _rad2dd(self):
+        """Convert radians to decimal degrees."""
+        self.decimal_degrees = self.radians * 180/pi
+        
+    def _dms2dd(self):
+        """Convert degrees, minutes, seconds to decimal degrees."""
+        print self.degrees
+        self.decimal_degrees = self.degrees + self.minutes/60 \
                                + self.seconds/3600
+    def _simplify(self):
+        print self.decimal_degrees
+        if self.decimal_degrees >= 360:
+            n_rotations = -int(self.decimal_degrees / 360)
+            self.decimal_degrees = self.decimal_degrees + 360 * n_rotations
+        elif self.decimal_degrees < 0:
+            if self.decimal_degrees <= -360:
+                n_rotations = -int(self.decimal_degrees / 360)
+                self.decimal_degrees = self.decimal_degrees + 360 * n_rotations
+            else:
+                self.decimal_degrees = self.decimal_degrees + 360
     
-    def _minutes_changed(self):
-        self.decimal_degrees = self.degrees + self.minutes/60.0 \
-                               + self.seconds/3600
+    def _anytrait_changed(self, name, new):
+        if name == 'decimal_degrees':
+#            self._simplify()
+            self._dd2rad()
+            self._dd2dms()
+        elif name =='radians':
+            self._rad2dd()
+#            self._simplify()
+#            self._dd2rad()
+#            self._dd2dms()
+        elif name in ('degrees', 'minutes', 'seconds'):
+            self._dms2dd()
+#            self._simplify()
+#            self._dd2dms()
+#            self._dd2rad()
 
-    def _seconds_changed(self):
-        self.decimal_degrees = self.degrees + self.minutes/60.0 \
-                               + self.seconds/3600
-    
-#    def _radians_changed(self):
-#        self.decimal_degrees = self.radians * 180/pi
 
 class MeasuredPoint(HasTraits):
     """A measurement.
@@ -129,7 +125,9 @@ def gui():
     logger = logging.getLogger()
     logger.addHandler(logging.StreamHandler(file('cogo.log', 'w')))
     logger.setLevel(logging.DEBUG)
-
+    
+#    from enthought.developer.helper.fbi import bp; bp()
+    
     angle = Angle()
     angle.configure_traits()
 #    point = MeasuredPoint()
