bdf4370f5fd0734d589cf93d58833683480eab61
Jed Frechette <jdfrech@unm.edu>
2009-05-04 13:47:01 -0600

    ENH: Add color output of warnings.

--- field_book_util.py
+++ field_book_util.py
@@ -10,9 +10,11 @@ __license__ = "MIT <http://opensource.org/licenses/mit-license.php>"
 from cogo import AngleDMS, avg_angles, dd2dms
 
 # Standard library imports.
-from copy import copy
 import csv
 import datetime
+import re
+import sys
+from copy import copy
 from glob import glob
 from optparse import OptionParser
 from os import name, path
@@ -23,6 +25,154 @@ from numpy import empty, mean, ptp
 # Enthought library imports.
 from enthought.traits.api import Date, Dict, Bool, Enum, File, Float, \
                                  HasTraits, Instance, Int, List, String, Time
+
+class TerminalController:
+    """
+    A class that can be used to portably generate formatted output to
+    a terminal.  
+    
+    `TerminalController` defines a set of instance variables whose
+    values are initialized to the control sequence necessary to
+    perform a given action.  These can be simply included in normal
+    output to the terminal:
+
+        >>> term = TerminalController()
+        >>> print 'This is '+term.GREEN+'green'+term.NORMAL
+
+    Alternatively, the `render()` method can used, which replaces
+    '${action}' with the string required to perform 'action':
+
+        >>> term = TerminalController()
+        >>> print term.render('This is ${GREEN}green${NORMAL}')
+
+    If the terminal doesn't support a given action, then the value of
+    the corresponding instance variable will be set to ''.  As a
+    result, the above code will still work on terminals that do not
+    support color, except that their output will not be colored.
+    Also, this means that you can test whether the terminal supports a
+    given action by simply testing the truth value of the
+    corresponding instance variable:
+
+        >>> term = TerminalController()
+        >>> if term.CLEAR_SCREEN:
+        ...     print 'This terminal supports clearning the screen.'
+
+    Finally, if the width and height of the terminal are known, then
+    they will be stored in the `COLS` and `LINES` attributes.
+    
+    Copied from the Python Cookbook Recipe 475116:
+    Using terminfo for portable color output & cursor control
+    """
+    # Cursor movement:
+    BOL = ''             #: Move the cursor to the beginning of the line
+    UP = ''              #: Move the cursor up one line
+    DOWN = ''            #: Move the cursor down one line
+    LEFT = ''            #: Move the cursor left one char
+    RIGHT = ''           #: Move the cursor right one char
+
+    # Deletion:
+    CLEAR_SCREEN = ''    #: Clear the screen and move to home position
+    CLEAR_EOL = ''       #: Clear to the end of the line.
+    CLEAR_BOL = ''       #: Clear to the beginning of the line.
+    CLEAR_EOS = ''       #: Clear to the end of the screen
+
+    # Output modes:
+    BOLD = ''            #: Turn on bold mode
+    BLINK = ''           #: Turn on blink mode
+    DIM = ''             #: Turn on half-bright mode
+    REVERSE = ''         #: Turn on reverse-video mode
+    NORMAL = ''          #: Turn off all modes
+
+    # Cursor display:
+    HIDE_CURSOR = ''     #: Make the cursor invisible
+    SHOW_CURSOR = ''     #: Make the cursor visible
+
+    # Terminal size:
+    COLS = None          #: Width of the terminal (None for unknown)
+    LINES = None         #: Height of the terminal (None for unknown)
+
+    # Foreground colors:
+    BLACK = BLUE = GREEN = CYAN = RED = MAGENTA = YELLOW = WHITE = ''
+    
+    # Background colors:
+    BG_BLACK = BG_BLUE = BG_GREEN = BG_CYAN = ''
+    BG_RED = BG_MAGENTA = BG_YELLOW = BG_WHITE = ''
+    
+    _STRING_CAPABILITIES = """
+    BOL=cr UP=cuu1 DOWN=cud1 LEFT=cub1 RIGHT=cuf1
+    CLEAR_SCREEN=clear CLEAR_EOL=el CLEAR_BOL=el1 CLEAR_EOS=ed BOLD=bold
+    BLINK=blink DIM=dim REVERSE=rev UNDERLINE=smul NORMAL=sgr0
+    HIDE_CURSOR=cinvis SHOW_CURSOR=cnorm""".split()
+    _COLORS = """BLACK BLUE GREEN CYAN RED MAGENTA YELLOW WHITE""".split()
+    _ANSICOLORS = "BLACK RED GREEN YELLOW BLUE MAGENTA CYAN WHITE".split()
+
+    def __init__(self, term_stream=sys.stdout):
+        """
+        Create a `TerminalController` and initialize its attributes
+        with appropriate values for the current terminal.
+        `term_stream` is the stream that will be used for terminal
+        output; if this stream is not a tty, then the terminal is
+        assumed to be a dumb terminal (i.e., have no capabilities).
+        """
+        # Curses isn't available on all platforms
+        try: import curses
+        except: return
+
+        # If the stream isn't a tty, then assume it has no capabilities.
+        if not term_stream.isatty(): return
+
+        # Check the terminal type.  If we fail, then assume that the
+        # terminal has no capabilities.
+        try: curses.setupterm()
+        except: return
+
+        # Look up numeric capabilities.
+        self.COLS = curses.tigetnum('cols')
+        self.LINES = curses.tigetnum('lines')
+        
+        # Look up string capabilities.
+        for capability in self._STRING_CAPABILITIES:
+            (attrib, cap_name) = capability.split('=')
+            setattr(self, attrib, self._tigetstr(cap_name) or '')
+
+        # Colors
+        set_fg = self._tigetstr('setf')
+        if set_fg:
+            for i,color in zip(range(len(self._COLORS)), self._COLORS):
+                setattr(self, color, curses.tparm(set_fg, i) or '')
+        set_fg_ansi = self._tigetstr('setaf')
+        if set_fg_ansi:
+            for i,color in zip(range(len(self._ANSICOLORS)), self._ANSICOLORS):
+                setattr(self, color, curses.tparm(set_fg_ansi, i) or '')
+        set_bg = self._tigetstr('setb')
+        if set_bg:
+            for i,color in zip(range(len(self._COLORS)), self._COLORS):
+                setattr(self, 'BG_'+color, curses.tparm(set_bg, i) or '')
+        set_bg_ansi = self._tigetstr('setab')
+        if set_bg_ansi:
+            for i,color in zip(range(len(self._ANSICOLORS)), self._ANSICOLORS):
+                setattr(self, 'BG_'+color, curses.tparm(set_bg_ansi, i) or '')
+
+    def _tigetstr(self, cap_name):
+        # String capabilities can include "delays" of the form "$<2>".
+        # For any modern terminal, we should be able to just ignore
+        # these, so strip them out.
+        import curses
+        cap = curses.tigetstr(cap_name) or ''
+        return re.sub(r'\$<\d+>[/*]?', '', cap)
+
+    def render(self, template):
+        """
+        Replace each $-substitutions in the given template string with
+        the corresponding terminal control string (if it's defined) or
+        '' (if it's not).
+        """
+        return re.sub(r'\$\$|\${\w+}', self._render_sub, template)
+
+    def _render_sub(self, match):
+        s = match.group()
+        if s == '$$': return s
+        else: return getattr(self, s[2:-1])
 
 class TPSModel(HasTraits):
     """Define total station model and specifications.""" 
@@ -407,6 +557,8 @@ def average_code_obs(in_book,
                   horizontal_tol='0:1:0.0', vertical_tol='0:1:0.0',
                   distance_tol=0.01):
     """Average observations with the same code."""
+    term = TerminalController()
+                
     out_book = copy(in_book)
     out_book.point_count = 0
     out_book.record_list = []
@@ -457,21 +609,27 @@ def average_code_obs(in_book,
                    range_horizontal.decimal_degrees]) > angle_tol.decimal_degrees:
                 print u'WARNING: Horizontal angle tolerance (%s) exceeded.' \
                         % angle_tol
-                print u'%s HAR difference: %s\n' % (avg_record.code,
-                                                    range_horizontal)
+                print u'%s HAR difference: %s%s%s\n' % (avg_record.code,
+                                                        term.RED,
+                                                        range_horizontal,
+                                                        term.NORMAL)
             d, m, s = vertical_tol.split(':')
             angle_tol = AngleDMS(degrees=int(d),
                                  minutes=int(m),
                                  seconds=float(s))
             if range_vertical.decimal_degrees > angle_tol.decimal_degrees:
                 print u'WARNING: Zenith angle tolerance (%s) exceeded.' % angle_tol
-                print u'%s ZA difference: %s\n' % (avg_record.code,
-                                                   range_vertical)
+                print u'%s ZA difference: %s%s%s\n' % (avg_record.code,
+                                                       term.YELLOW,
+                                                       range_vertical,
+                                                       term.NORMAL)
             if range_distance > distance_tol:
                 print 'WARNING: Slope distance tolerance (%.4f) exceeded.' \
                        % distance_tol
-                print '%s S difference: %.4f\n' % (avg_record.code,
-                                                   range_distance)
+                print '%s S difference: %s%.4f%s\n' % (avg_record.code,
+                                                       term.MAGENTA,
+                                                       range_distance,
+                                                       term.NORMAL)
             out_book.record_list.append(avg_record)
             out_book.point_count += 1
         else:
@@ -485,11 +643,10 @@ if __name__ == '__main__':
         BOOK = SOKKIABook()
         BOOK.load(in_filename)
         AVG_BOOK = average_code_obs(BOOK)
-#        AVG_BOOK.export_hor_obs('%s.obs' % \
-#                                path.splitext(path.basename(in_filename))[0],
-#                                bs_station='m3a',
-#                                hor_offset= -0.0319444)
-        AVG_BOOK.export_azimuth_obs('%s.obs' % \
-                                    path.splitext(path.basename(in_filename))[0])
+        AVG_BOOK.export_hor_obs('%s.obs' % \
+                                path.splitext(path.basename(in_filename))[0],
+                                bs_station='north')
+#        AVG_BOOK.export_azimuth_obs('%s.obs' % \
+#                                    path.splitext(path.basename(in_filename))[0])
 #        AVG_BOOK.export_direction_obs('%s.obs' % \
 #                                      path.splitext(path.basename(in_filename))[0])
