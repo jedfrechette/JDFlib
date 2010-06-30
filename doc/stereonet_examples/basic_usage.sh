d17e786ebf626ba9b071d8627a2062824df2106d
Jed Frechette <jdfrech@unm.edu>
2010-06-30 11:30:49 -0600

    ENH: Update examples to work without GMT tools in path.

--- doc/stereonet_examples/basic_usage.sh
+++ doc/stereonet_examples/basic_usage.sh
@@ -8,30 +8,31 @@ R='-Rd'
 J='-JA0/0/6i'
 B='-Bg10'
 outfile='BasicStereonet.ps'
+gmt_bin='/usr/lib/gmt/bin'
 
 #--Set up a basemap-----------------------------------------
-psbasemap $R $J $B:."Example Stereonet": -P -K > $outfile
+$gmt_bin/psbasemap $R $J $B:."Example Stereonet": -P -K > $outfile
 
 #--Plot a plane striking 320 and dipping 30 degrees to the NE 
 #  (320/30NE follows the RHR, so the direction is optional)
 #  Note that psxy needs the -M (multisegment) option when plotting planes
-echo 320/30 | stereonet | psxy $R $J -W2p/blue -M -O -K >> $outfile
+echo 320/30 | stereonet | $gmt_bin/psxy $R $J -W2p/blue -m -O -K >> $outfile
 
 #--Plot the pole to the same plane--------------------------
 #  This time we'll use an equivalent quadrant measurement
 #  The input can be formatted in multiple ways, the difference
 #  here is the --poles option, not the different input format.
-echo S40E/30E | stereonet --poles | psxy $R $J -Sc5p -Gblue -O -K >> $outfile
+echo S40E/30E | stereonet --poles | $gmt_bin/psxy $R $J -Sc5p -Gblue -O -K >> $outfile
 
 #--Plot a rake along the plane------------------------------
 #  This is a line (point on a stereonet) raking 45 degrees down
 #  from the northwest end of the plane.  The plane is denoted
 #  using yet another equivalent formatting. 
-echo 140/30NE 45NW | stereonet --rakes | psxy $R $J -Sa10p -Gred -O -K >> $outfile
+echo 140/30NE 45NW | stereonet --rakes | $gmt_bin/psxy $R $J -Sa10p -Gred -O -K >> $outfile
 
 #--Plot the bisector of the plane and its pole--------------
 #  This just to illustrate plotting the plunge/bearing of linear
 #  measurements.  Note that plunge comes first! 
-echo 75/050 | stereonet --lines | psxy $R $J -Sc5p -Ggreen -O >> $outfile
+echo 75/050 | stereonet --lines | $gmt_bin/psxy $R $J -Sc5p -Ggreen -O >> $outfile
 
-gs $outfile
+#gs $outfile
--- doc/stereonet_examples/test_stereonet_unfolding.sh
+++ doc/stereonet_examples/test_stereonet_unfolding.sh
@@ -7,16 +7,17 @@ J=-JA0/0/6i
 B=-Bg10
 
 outfile=junk.ps
+gmt_bin='/usr/lib/gmt/bin'
 
 line=10/350
 Hplane=190/10
 Dplane=130/45
 
-psbasemap $R $J $B -K -P > $outfile
-echo $line | stereonet --lines | psxy $R $J -O -K -Sc5p -Ggreen>> $outfile
-echo $Hplane | stereonet | psxy $R $J -O -K -M -W2p/blue >> $outfile
-echo $Dplane | stereonet | psxy $R $J -O -K -M -W2p/green >> $outfile
-echo $line H $Hplane | stereonet --lines | psxy $R $J -O -K -Sa10p -Gred >> $outfile
-echo $Dplane H $Hplane | stereonet | psxy $R $J -O -M -W2p/red >> $outfile
+$gmt_bin/psbasemap $R $J $B -K -P > $outfile
+echo $line | stereonet --lines | $gmt_bin/psxy $R $J -O -K -Sc5p -Ggreen>> $outfile
+echo $Hplane | stereonet | $gmt_bin/psxy $R $J -O -K -m -W2p/blue >> $outfile
+echo $Dplane | stereonet | $gmt_bin/psxy $R $J -O -K -m -W2p/green >> $outfile
+echo $line H $Hplane | stereonet --lines | $gmt_bin/psxy $R $J -O -K -Sa10p -Gred >> $outfile
+echo $Dplane H $Hplane | stereonet | $gmt_bin/psxy $R $J -O -m -W2p/red >> $outfile
 
-okular $outfile
+#okular $outfile
