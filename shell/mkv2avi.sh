dccca44939128c4104645ac58a997e008155cba4
Jed Frechette <jed@lidarguys.com>
2011-09-22 21:05:01 -0600

    Fix audio sync.

--- shell/mkv2avi.sh
+++ shell/mkv2avi.sh
@@ -2,10 +2,11 @@
 VIDEOCODEC="Xvid"
 AUDIOCODEC="MP3"
 IFS="|"
-#echo $@
 for INFILE in $@ ; do
   BASE=$(basename $INFILE)
   OUTFILE=${BASE%.*}.avi
-#  echo $OUTFILE
-  avidemux2_cli --video-codec $VIDEOCODEC --audio-codec $AUDIOCODEC --force-alt-h264 --load "$INFILE" --save "$OUTFILE" --quit
+  # This will still prompt the user for each file.
+  # Answer No, don't use alternate h264 read mode.
+  # Answer Yes, rebuild index.
+  avidemux2_cli --video-codec $VIDEOCODEC --audio-codec $AUDIOCODEC --autoindex --rebuild-index --force-b-frame --load "$INFILE" --save "$OUTFILE" --quit
 done
