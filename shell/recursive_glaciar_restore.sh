304cb5475eaf3cbde122ce0aa63057574be38a11
Jed Frechette <jed@lidarguys.com>
2014-10-23 17:54:56 -0600

    BUG: Fix typos.

--- shell/recursive_glaciar_restore.sh
+++ shell/recursive_glaciar_restore.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # This will give you a nice list of all objects in the bucket with the bucket name stripped out
-s3cmd ls -r s3://lidarguys-inactive-projects/BOE | awk '{print $4}' | sed 's#s3://<your-bucket-name>/##' > glacier-restore.txt
+s3cmd ls -r s3://lidarguys-inactive-projects/BOE | awk '{print $4}' | sed 's#s3://lidarguys-inactive-projects/##' > glacier-restore.txt
 
 for x in `cat glacier-restore.txt`
 do
     echo "restoring $x"
-    aws s3api restore-object --restore-request Days=7 --bucket lidarguys-inactive-projects/BOE --key "$x"
+    aws s3api restore-object --restore-request Days=7 --bucket lidarguys-inactive-projects --key "$x"
 done
